d0c02a6f3dbed31ba480e499da29a6165e7a3903
peteratnsight <pp@n-sight.de>
2024-02-25 14:19:05 +0100

    Create application.js

---
+++ application.js
@@ -0,0 +1,19 @@
+class APP {
+  constructor(config) {
+    this.name = 'TM Application';
+    this.version = '1.0';
+    this.defaultConfig = config;
+  }
+
+  sayHello() {
+    console.log(`Hello World`);
+  }
+
+  getConfig() {
+    return this.config;
+  }
+
+  initRuntimeConfig(runtimeConfig) {
+    this.defaultConfig = Object.assign({}, this.defaultConfig, runtimeConfig);
+  }
+}
