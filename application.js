0b642956945e7845d4be38e0198f178055dfbe9a
peteratnsight <pp@n-sight.de>
2024-02-25 14:47:09 +0100

    Update application.js

--- application.js
+++ application.js
@@ -1,4 +1,8 @@
 class TMApplication {
+  const defaultConfig = {
+    buttons: {},
+    pillClasses: []
+  }
   constructor(config) {
     this.name = 'TM Application';
     this.version = '1.0';
@@ -12,8 +16,44 @@ class TMApplication {
   getConfig() {
     return this.config;
   }
+  
+  getConfigValue(key) {
+    if(this.defaultConfig[key] == undefined || this.defaultConfig[key] === false) return '';
+    return this.defaultConfig[key];
+  }
+
+  executeStylesOnElement(configStylesName, element) {
+    let thisConfiguration = this.defaultConfig[configStylesName];
+    if(thisConfiguration == undefined) return element;
+    for (const [key, value] of Object.entries(thisConfiguration)) {
+      element.style[key] = value;
+    }
+    return element;
+  }
 
   initRuntimeConfig(runtimeConfig) {
+    if(runtimeConfig == undefined) return false;
     this.defaultConfig = Object.assign({}, this.defaultConfig, runtimeConfig);
+  }
+
+  init(runtimeConfig) {
+    initRuntimeConfig(runtimeConfig);
+    initSetup();
+    initializeConfigButtton();
+    if(this.defaultConfig.buttons != undefined && this.defaultConfig.buttons.length != 0) initilizeApplicationButtons(this.defaultConfig.buttons);
+  }
+
+  initSetup() { return true; }
+  initializeConfigButtton() { return true; }
+  initilizeApplicationButtons(buttons) { return true; }
+
+  getButton(funct,btnText,runtimeConfig) {
+    if(runtimeConfig != undefined) initRuntimeConfig(runtimeConfig);
+    let pill = document.createElement("button");
+    pill.textContent = btnText;
+    pill.classList.add(...getConfigValue('pillClasses'));
+    pill = executeStylesOnElement('pillStyles',pill);
+    pill.addEventListener('click', () => funct());
+    return pill;
   }
 }
